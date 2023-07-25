2ce5a3dde4e0ce52585cf99796a935c4cc07a801
pifragile <pigu@bluewin.ch>
2023-07-25 17:59:50 +0200

    retainV2

---
+++ index.js
@@ -0,0 +1,31 @@
+let graphics;
+let graphicsSize = 2000;
+let canvasSize;
+
+function setup() {
+    pg = createGraphics(graphicsSize, graphicsSize);
+    canvasSize = min(windowHeight, windowWidth);
+    createCanvas(canvasSize, canvasSize);
+
+    noLoop();
+}
+
+function draw() {
+    pg.noStroke();
+    for (let i = 0; i < 20; i++) {
+        pg.fill(randRT() * 255, 0, 0);
+        pg.circle(
+            randRT() * graphicsSize,
+            randRT() * graphicsSize,
+            randRT() * 0.1 * graphicsSize
+        );
+    }
+    image(pg, 0, 0, canvasSize, canvasSize);
+    triggerPreview()
+}
+
+function windowResized() {
+    let canvasSize = min(windowHeight, windowWidth);
+    resizeCanvas(canvasSize, canvasSize, true);
+    image(pg, 0, 0, canvasSize, canvasSize);
+}
---
+++ retain.js
@@ -0,0 +1,71 @@
+function sfc32(a, b, c, d) {
+    return function () {
+        a >>>= 0;
+        b >>>= 0;
+        c >>>= 0;
+        d >>>= 0;
+        var t = (a + b) | 0;
+        a = b ^ (b >>> 9);
+        b = (c + (c << 3)) | 0;
+        c = (c << 21) | (c >>> 11);
+        d = (d + 1) | 0;
+        t = (t + d) | 0;
+        c = (c + t) | 0;
+        return (t >>> 0) / 4294967296;
+    };
+}
+
+function cyrb128(str) {
+    let h1 = 1779033703,
+        h2 = 3144134277,
+        h3 = 1013904242,
+        h4 = 2773480762;
+    for (let i = 0, k; i < str.length; i++) {
+        k = str.charCodeAt(i);
+        h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
+        h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
+        h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
+        h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
+    }
+    h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
+    h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
+    h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
+    h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
+    return [
+        (h1 ^ h2 ^ h3 ^ h4) >>> 0,
+        (h2 ^ h1) >>> 0,
+        (h3 ^ h1) >>> 0,
+        (h4 ^ h1) >>> 0,
+    ];
+}
+
+const cyrb53 = (str, seed = 0) => {
+    let h1 = 0xdeadbeef ^ seed,
+        h2 = 0x41c6ce57 ^ seed;
+    for (let i = 0, ch; i < str.length; i++) {
+        ch = str.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+    h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+    h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+
+    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+};
+
+function getRNG(hash) {
+    return sfc32(...cyrb128(hash));
+}
+
+window.capturePreview = false;
+
+function triggerPreview() {
+    window.capturePreview = true;
+    console.log("Trigger Preview");
+}
+
+const randRT = getRNG(hash);
+
+const randomSeedRT = cyrb53(hash);
